4a4cf31f4e2196522a8809857b1458104239ba21
KuroInit <ashwinblgps2013@gmail.com>
2025-02-28 15:07:47 +0800

    update testing

---
+++ web_server/testing.py
@@ -0,0 +1,11 @@
+import cv2
+from ultralytics import YOLO
+
+MODEL_PATH = "web_server/utils/trained_models/v8_white_bg.onnx"
+model = YOLO(MODEL_PATH)
+img = cv2.imread("test.jpg")  # Use a known-good image file.
+if img is None:
+    print("Image load failed!")
+else:
+    results = model(img, verbose=False)
+    print("Inference complete. Results:", results)
