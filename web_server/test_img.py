cfe4bf22756be9a06d0688c5e5cc75a0de9ce298
Jaron <limksjaron123@gmail.com>
2025-03-21 12:58:55 +0800

    update test_img.py

--- web_server/test_img.py
+++ web_server/test_img.py
@@ -5,7 +5,6 @@ CAPTURED_IMAGE_PATH = "test_capture.jpg"
 
 def capture_image():
     try:
-        # Capture image using libcamera-jpg with a 1000ms timeout.
         cmd = ["libcamera-jpg", "-o", CAPTURED_IMAGE_PATH, "--timeout", "1000"]
         subprocess.run(cmd, check=True)
         print("Image captured using libcamera-jpg.")
@@ -16,11 +15,9 @@ def capture_image():
 
 if __name__ == "__main__":
     if capture_image():
-        # Load the captured image using OpenCV.
         img = cv2.imread(CAPTURED_IMAGE_PATH)
         if img is None or img.size == 0:
             print("Failed to load captured image.")
         else:
             print("Image loaded successfully.")
             print("Image shape:", img.shape)
-            # Display the image (if you are in an environment that supports a display)
